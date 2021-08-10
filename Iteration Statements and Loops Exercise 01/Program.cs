14759774b4b59bd38c357e13e86e4254a187ab2f
johndward01 <johndward01@gmail.com>
2021-08-09 20:49:45 -0500

    init

--- Iteration Statements and Loops Exercise 01/Program.cs
+++ Iteration Statements and Loops Exercise 01/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿
 
-namespace Iteration_Statements_and_Loops_Exercise_01
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Iteration_Statements_and_Loops_Exercise_01_Answers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] arr1 = new int[] { 1, 7, 9, 4, 2, 3, 8, 10 };
+            // TODO: Create a foreach loop to iterate through arr1 and print it's elements to the console
+
+
+            AddSpaces();
+
+
+            // TODO: Create a for loop to iterate through arr1 IN REVERSE ORDER and print it's elements to the console
+
+
+            AddSpaces();
+
+
+            var userInput = "";
+            var numbers = new List<int>();
+            // TODO: Create a do-while loop that asks the user for a number and then adds it to a list called numbers
+            // TODO: Make the do-while loop terminate when the user types 'exit' 
+
+
+            AddSpaces();
+
+
+            var index = 0;
+            // TODO: Create a while loop that prints out all the nums inside numbers list to the console
+
+
+
+        }
+
+        public static void AddSpaces()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
